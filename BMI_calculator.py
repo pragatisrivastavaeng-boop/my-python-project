ced37ab77ddb27e92420f2b82ac60aafb97dffee
Pragati Srivastava <pragati.srivastavaeng@gmail.com>
2026-05-13 20:52:11 +0530

    Create BMI_calculator.py

---
+++ BMI_calculator.py
@@ -0,0 +1,27 @@
+bmi = 84 / 1.65 ** 2
+
+print(bmi)
+
+print(int(bmi))
+
+print(round(bmi))
+
+print(round(7.9) )
+
+print(round(bmi,3))
+
+
+score = 0
+
+score +=1
+
+print(score)
+
+#f-strings
+print("your score is " + str(score))
+
+
+score = 0
+height = 1.8
+is_winning = True
+print(f"your score is= {score} ,your height is {height}. you are winning is{True}")
